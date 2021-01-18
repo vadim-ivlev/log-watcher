f252e3f5714098ec74e7dfca2b25b3e254914a46
vadim-ivlev <vadim.ivlev@gmail.com>
2021-01-18 16:54:28 +0300

    .

--- sh/greetings.sh
+++ sh/greetings.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo
-echo "L O G  -  M O N I T O R"
+echo "L O G  -  W A T C H E R"
 echo
 # echo "* Logstash () http://localhost:5044"
 # echo "* Elasticsearch (full text search) http://localhost:9200"
 # echo "* Kibana (visualize logs) http://localhost:5601"
 # echo "* Cerebro (visualize cluster) http://localhost:9000"
 # echo
-echo "* Caddy (proxy to Kibana)    http://localhost:9094"
+# echo "* Caddy (proxy to Kibana)    http://localhost:9094"
 # echo "* Caddy (proxy to Cerebro) http://localhost:9094/cerebro/#/connect"
-echo "* Caddy (proxy to Portainer) http://localhost:9094/portainer/"
-echo "* Caddy (proxy to www)       http://localhost:9094/www/"
+# echo "* Caddy (proxy to Portainer) http://localhost:9094/portainer/"
+# echo "* Caddy (proxy to www)       http://localhost:9094/www/"
